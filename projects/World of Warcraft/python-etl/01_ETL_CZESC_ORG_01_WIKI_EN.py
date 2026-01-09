aba99289b5d6d693d27126e3e9273b2fdb162931
Loe-Aner <146349544+Loe-Aner@users.noreply.github.com>
2026-01-09 23:17:46 +0100

    Test commitu z VSC

--- projects/World of Warcraft/python-etl/01_ETL_CZESC_ORG_01_WIKI_EN.py
+++ projects/World of Warcraft/python-etl/01_ETL_CZESC_ORG_01_WIKI_EN.py
@@ -20,14 +20,14 @@ from scraper_wiki_main import parsuj_misje_z_url, dekompresuj_html
 # BAZA
 kategorie = [
     #"https://warcraft.wiki.gg/wiki/Category:Quests_at_80",
-    "https://warcraft.wiki.gg/wiki/Category:Quests_at_80-83",
-    "https://warcraft.wiki.gg/wiki/Category:Quests_at_80-90",
-    "https://warcraft.wiki.gg/wiki/Category:Quests_at_83",
-    "https://warcraft.wiki.gg/wiki/Category:Quests_at_83-88",
-    "https://warcraft.wiki.gg/wiki/Category:Quests_at_88-90",
-    "https://warcraft.wiki.gg/wiki/Category:Quests_at_1-10",
+    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_80-83",
+    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_80-90",
+    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_83",
+    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_83-88",
+    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_88-90",
+    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_1-10",
     #"https://warcraft.wiki.gg/wiki/Category:Quests_at_2",
-    #"https://warcraft.wiki.gg/wiki/Category:Quests_at_6-10",
+    "https://warcraft.wiki.gg/wiki/Category:Quests_at_6-10",
     #"https://warcraft.wiki.gg/wiki/Category:Quests_at_7-10",
     #"https://warcraft.wiki.gg/wiki/Category:Quests_at_10"
 ]
--- projects/World of Warcraft/sql-tabele/92_zwracanie_nazw_npc.sql
+++ projects/World of Warcraft/sql-tabele/92_zwracanie_nazw_npc.sql
@@ -1,18 +1,34 @@
 WITH idki AS (
-SELECT DISTINCT
-    tabela_wartosci.ID_NPC
-FROM dbo.MISJE AS m
-CROSS APPLY (
-    VALUES
-        (m.NPC_START_ID),
-        (m.NPC_KONIEC_ID)
-) AS tabela_wartosci (ID_NPC)
-WHERE m.MISJA_ID_MOJE_PK = 3
-) -- fajna alternatywa dla unpivot
+    SELECT DISTINCT
+        tabela_wartosci.ID_NPC
+    FROM dbo.MISJE AS m
+    CROSS APPLY (
+        VALUES
+            (m.NPC_START_ID),
+            (m.NPC_KONIEC_ID)
+    ) AS tabela_wartosci (ID_NPC)
+    WHERE m.MISJA_ID_MOJE_PK = 3
+),
 
-SELECT i.ID_NPC, ns.NAZWA
-FROM idki AS i
-INNER JOIN dbo.NPC_STATUSY AS ns
-  ON i.ID_NPC = ns.NPC_ID_FK
-WHERE ns.STATUS = '3_ZATWIERDZONO'
-;
+oczyszczone_dane AS (
+    SELECT 
+        i.ID_NPC,
+        ns.STATUS,
+        CASE 
+            WHEN CHARINDEX('[', ns.NAZWA) > 0 
+            THEN RTRIM(LEFT(ns.NAZWA, CHARINDEX('[', ns.NAZWA) - 1))
+            ELSE ns.NAZWA 
+        END AS CZYSTA_NAZWA
+    FROM idki AS i
+    INNER JOIN dbo.NPC_STATUSY AS ns
+        ON i.ID_NPC = ns.NPC_ID_FK
+)
+
+SELECT 
+    pvt.[0_ORYGINA£], 
+    pvt.[3_ZATWIERDZONO]
+FROM oczyszczone_dane
+PIVOT (
+    MAX(CZYSTA_NAZWA)
+    FOR STATUS IN ([0_ORYGINA£], [3_ZATWIERDZONO])
+) AS pvt
