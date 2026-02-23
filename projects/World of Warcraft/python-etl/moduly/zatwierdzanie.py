2a63f85fcbea4d42a09b9ba2061659a25b3707bb
Loe-Aner <py.piotrx@gmail.com>
2026-02-23 20:19:01 +0100

    drobne zmiany w skrypcie i kilkaset przetlumaczonych wypowiedzi, #86, #84

--- projects/World of Warcraft/python-etl/moduly/zatwierdzanie.py
+++ projects/World of Warcraft/python-etl/moduly/zatwierdzanie.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 from moduly.utils import sklej_warunki_w_WHERE
 
-
 def stworz_excele_do_zatwierdzenia_tlumaczen(silnik, kraina = None, fabula = None, dodatek = None, sciezka = None):
 
     warunki_sql = sklej_warunki_w_WHERE(kraina, fabula, dodatek)
@@ -21,15 +20,32 @@ def stworz_excele_do_zatwierdzenia_tlumaczen(silnik, kraina = None, fabula = Non
     """)
 
     q_select_tytul = text(f"""
-        SELECT MISJA_ID_MOJE_PK, MISJA_TYTUL_EN, MISJA_TYTUL_PL, NAZWA_LINII_FABULARNEJ_EN, NAZWA_LINII_FABULARNEJ_PL
-        FROM dbo.MISJE
+        SELECT 
+            m.MISJA_ID_MOJE_PK, 
+            m.MISJA_TYTUL_EN, 
+            m.MISJA_TYTUL_PL, 
+            m.NAZWA_LINII_FABULARNEJ_EN, 
+            m.NAZWA_LINII_FABULARNEJ_PL,
+            ns1.NAZWA AS NAZWA_NPC_START,
+            ns2.NAZWA AS NAZWA_NPC_KONIEC
+        FROM dbo.MISJE AS m
+        INNER JOIN dbo.NPC_STATUSY AS ns1
+           ON ns1.NPC_ID_FK = m.NPC_START_ID
+          AND ns1.STATUS = '3_ZATWIERDZONO'
+        LEFT OUTER JOIN dbo.NPC_STATUSY AS ns2
+          ON ns2.NPC_ID_FK = m.NPC_KONIEC_ID
+         AND ns2.STATUS = '3_ZATWIERDZONO'
         WHERE MISJA_ID_MOJE_PK IN :misje_id
     ;
     """).bindparams(bindparam("misje_id", expanding=True))
 
     q_select_misje_dialogi = text("""
-        SELECT MISJA_ID_MOJE_FK, SEGMENT, STATUS, NR_BLOKU_DIALOGU, NR_WYPOWIEDZI, TRESC
-        FROM [dbo].[DIALOGI_STATUSY]
+        SELECT 
+        ds.MISJA_ID_MOJE_FK, ds.SEGMENT, ds.STATUS, ds.NR_BLOKU_DIALOGU, ds.NR_WYPOWIEDZI, ds.TRESC, ns.NAZWA AS NAZWA_NPC_START
+        FROM [dbo].[DIALOGI_STATUSY] AS ds
+        LEFT OUTER JOIN dbo.NPC_STATUSY AS ns
+          ON ns.NPC_ID_FK = ds.NPC_ID_FK
+         AND ns.STATUS = '3_ZATWIERDZONO'
         WHERE MISJA_ID_MOJE_FK IN :misje_id
     """).bindparams(bindparam("misje_id", expanding=True))
 
@@ -55,6 +71,10 @@ def stworz_excele_do_zatwierdzenia_tlumaczen(silnik, kraina = None, fabula = Non
     df_misje_dialogi = pd.DataFrame(misje_dialogi)
     df_misje_tresci = pd.DataFrame(misje_tresci)
 
+    # Słowniki mapujące NPC z pobranych tytułów
+    mapa_npc_start = df_tytuly.set_index("MISJA_ID_MOJE_PK")["NAZWA_NPC_START"].to_dict()
+    mapa_npc_koniec = df_tytuly.set_index("MISJA_ID_MOJE_PK")["NAZWA_NPC_KONIEC"].to_dict()
+
     nowe_naglowki = {
         "MISJA_ID_MOJE_PK": "MISJA_ID",
         "MISJA_ID_MOJE_FK": "MISJA_ID",
@@ -73,64 +93,105 @@ def stworz_excele_do_zatwierdzenia_tlumaczen(silnik, kraina = None, fabula = Non
     }
     
     kolejnosc_kolumn_koniec = [
-        "MISJA_ID", "STATUS", "SEGMENT", "PODSEGMENT", "ID_SEGMENTU", "NR_BLOKU", "NR_WYP", "TRESC"
+        "MISJA_ID", "SEGMENT", "PODSEGMENT", "ID_SEGMENTU", "NR_BLOKU", "NR_WYP", "STATUS", "TRESC", "NAZWA_NPC_START", "NAZWA_NPC_KONIEC"
     ]
 
     df_tytuly = (
         df_tytuly.melt(
-            ["MISJA_ID_MOJE_PK"], 
+            id_vars=["MISJA_ID_MOJE_PK", "NAZWA_NPC_START", "NAZWA_NPC_KONIEC"], 
             value_vars=["MISJA_TYTUL_EN", "MISJA_TYTUL_PL"], 
-            var_name="STATUS")
+            var_name="STATUS"
+        )
         .rename(columns=nowe_naglowki)
         .replace(nowe_wiersze)
         .assign(SEGMENT = "TYTUL", PODSEGMENT = "", NR_BLOKU = 1, NR_WYP = 1)
-        )
+    )
     
     df_misje_dialogi = (
         df_misje_dialogi
         .rename(columns=nowe_naglowki)
-        .assign(PODSEGMENT = "")
+        .assign(
+            PODSEGMENT = "",
+            NAZWA_NPC_KONIEC = lambda x: x["MISJA_ID"].map(mapa_npc_koniec)
+        )
+    )
+
+    df_misje_dialogi_zatwierdzone = (
+        df_misje_dialogi[["MISJA_ID", "SEGMENT", "PODSEGMENT", "NR_BLOKU", "NR_WYP", "NAZWA_NPC_START", "NAZWA_NPC_KONIEC"]]
+        .drop_duplicates()
+        .assign(STATUS="3_ZATWIERDZONO", TRESC="")
     )
 
     df_misje_tresci = (
         df_misje_tresci
         .rename(columns=nowe_naglowki)
-        .assign(NR_WYP = 1)
+        .assign(
+            NR_WYP = 1,
+            NAZWA_NPC_START = lambda x: x["MISJA_ID"].map(mapa_npc_start),
+            NAZWA_NPC_KONIEC = lambda x: x["MISJA_ID"].map(mapa_npc_koniec)
+        )
+    )
+
+    df_misje_tresci_zatwierdzone = (
+        df_misje_tresci[["MISJA_ID", "SEGMENT", "PODSEGMENT", "NR_BLOKU", "NR_WYP", "NAZWA_NPC_START", "NAZWA_NPC_KONIEC"]]
+        .drop_duplicates()
+        .assign(STATUS="3_ZATWIERDZONO", TRESC="")
     )
 
     df_polaczone = (
-            pd.concat([df_tytuly, df_misje_dialogi, df_misje_tresci])
+            pd.concat([
+                df_tytuly, 
+                df_misje_dialogi, df_misje_dialogi_zatwierdzone, 
+                df_misje_tresci, df_misje_tresci_zatwierdzone
+            ])
             .assign(
                 ID_SEGMENTU = lambda x: x["SEGMENT"].map(mapping_segmentow)
             )
-            .sort_values(by=["MISJA_ID", "STATUS", "ID_SEGMENTU", "NR_BLOKU", "NR_WYP"])
+            .sort_values(by=["MISJA_ID", "ID_SEGMENTU", "SEGMENT", "PODSEGMENT", "NR_BLOKU", "NR_WYP", "STATUS"])
             .reset_index(drop=True)
             [kolejnosc_kolumn_koniec]
         )
     
-    with pd.ExcelWriter(sciezka, engine="xlsxwriter") as zapis: # type: ignore
+    with pd.ExcelWriter(sciezka, engine="xlsxwriter") as zapis:
             df_polaczone.to_excel(zapis, sheet_name="Tlumaczenia", index=False)
             
             arkusz = zapis.sheets["Tlumaczenia"]
             
-            format_bazowy = zapis.book.add_format({ # type: ignore
+            arkusz.freeze_panes(1, 0)
+            
+            format_bazowy = zapis.book.add_format({
                 "bg_color": "black",
                 "font_color": "white",
                 "align": "center",
                 "valign": "vcenter"
             })
             
-            format_zawijania = zapis.book.add_format({ # type: ignore
+            format_zawijania = zapis.book.add_format({
                 "bg_color": "black",
                 "font_color": "white",
                 "align": "center",
                 "valign": "vcenter",
                 "text_wrap": True
             })
+
+            format_zatwierdzone = zapis.book.add_format({
+                "bg_color": "#404040",
+                "font_color": "white"
+            })
             
             arkusz.set_column("A:XFD", None, format_bazowy)
             
             arkusz.set_column("A:G", 15, format_bazowy)
             arkusz.set_column("H:H", 80, format_zawijania)
+            arkusz.set_column("I:J", 25, format_bazowy)
+
+            maks_wiersz = len(df_polaczone) + 1
+            zakres_formatowania = f"A2:J{maks_wiersz}"
+
+            arkusz.conditional_format(zakres_formatowania, {
+                "type": "formula",
+                "criteria": '=$G2="3_ZATWIERDZONO"',
+                "format": format_zatwierdzone
+            })
 
     return df_polaczone
