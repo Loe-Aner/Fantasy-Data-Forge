736e607e948f1b730151ebd6663a9c5b575208de
Loe-Aner <146349544+Loe-Aner@users.noreply.github.com>
2026-04-05 20:33:27 +0200

    nauka agentów AI ciąg dalszy; jeszcze przerwa od projektu dopóki nie zacznę ogarniać Langchain

--- projects/World of Warcraft/python-etl/13_ETL_CZESC_ZATW_PRZERZUC_01.py
+++ projects/World of Warcraft/python-etl/13_ETL_CZESC_ZATW_PRZERZUC_01.py
@@ -6,5 +6,5 @@ silnik = utworz_engine_do_db()
 
 zatwierdz_tlumaczenia(
     silnik,
-    sciezka_excel_zatwierdzenia("Call of the Goddess_ZATW_PRZE.xlsx")
+    sciezka_excel_zatwierdzenia("Eversong Intro_ZATW_PRZE.xlsx")
 )
